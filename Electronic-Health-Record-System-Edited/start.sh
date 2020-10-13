7ec619fb0fdc93943f32e5e505caf428d0c3822b
Kamesh-K <kamesh3799@gmail.com>
2020-10-13 17:07:05 +0530

    Adding initial set of files

---
+++ Electronic-Health-Record-System-Edited/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Commands required to instatiate the network and start the REST-API 
+composer archive create -t dir -n .
+composer network install --card PeerAdmin@hlfv1 --archiveFile ehr@0.0.1.bna
+# Use the following line if the CCENV is not pulled from docker 
+# docker pull hyperledger/fabric-ccenv:1.2.1
+docker tag hyperledger/fabric-ccenv:1.2.1 hyperledger/fabric-ccenv:latest
+composer network start --networkName ehr --networkVersion 0.0.1 --card PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw --file networkadmin.card
+composer-rest-server -c admin@ehr -n always -u true -d y -w true
